d46f6602c6c3de7f22f5c32f535726064b498e35
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-24 16:18:10 +0100

    Styled stories

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { number, string, shape } from 'prop-types';
 import './Story.css';
 
-const Story = ({ story }) => {
+const Story = ({ story, columns }) => {
   const { title, url, author, comments, points } = story;
 
   return (
     <div className="story">
-      <span>
+      <span style={{ width: columns.title.width }}>
         <a href={url}>{title}</a>
       </span>
-      <span>{author}</span>
-      <span>{comments}</span>
-      <span>{points}</span>
+      <span style={{ width: columns.author.width }}>{author}</span>
+      <span style={{ width: columns.comments.width }}>{comments}</span>
+      <span style={{ width: columns.points.width }}>{points}</span>
+      <span style={{ width: columns.archive.width }} />
     </div>
   );
 };
+
+const style = shape({
+  label: string,
+  width: string.isRequired,
+});
 
 Story.propTypes = {
   story: shape({
@@ -25,6 +31,13 @@ Story.propTypes = {
     comments: number.isRequired,
     points: number.isRequired,
     objectId: number.isRequired,
+  }).isRequired,
+  columns: shape({
+    title: style.isRequired,
+    author: style.isRequired,
+    comments: style.isRequired,
+    points: style.isRequired,
+    archive: style.isRequired,
   }).isRequired,
 };
 
