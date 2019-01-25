0b6aa8408bb53559252c11f61019e7f5b2f3d04c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-25 16:02:37 +0000

    Deploy amercier/react-redux-tutorial to github.com/amercier/react-redux-tutorial.git:gh-pages

---
+++ precache-manifest.d0789dcd661c076507b09694656b8739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "090670e6220df84b7ddf",
+    "url": "/static/js/main.090670e6.chunk.js"
+  },
+  {
+    "revision": "4f42397f44248a7efd23",
+    "url": "/static/js/1.4f42397f.chunk.js"
+  },
+  {
+    "revision": "090670e6220df84b7ddf",
+    "url": "/static/css/main.89b76872.chunk.css"
+  },
+  {
+    "revision": "4484acf684bb6ccf6a779f3595159e30",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4433dcfbe55ee3e194aad2783f41a13e.js"
+  "/precache-manifest.d0789dcd661c076507b09694656b8739.js"
 );
 
 workbox.clientsClaim();
